c10c0e2efd6e0efb506af2664de82a83641e98da
juan.aranda.galvis@gmail.com <juan.aranda.galvis@gmail.com@24724b2c-733d-aa55-8f23-991fbb591484>
2013-11-10 05:44:40 +0000

    Se agrega desarrllo: - Coneccion LDAP - Login a LDAP (local o remoto: parametros de conexion configurables en archivo .properties) - JPA para manejo de persistencia

--- OMS/src/com/kallSonys/managedBeans/LoginPageBean.java
+++ OMS/src/com/kallSonys/managedBeans/LoginPageBean.java
@@ -2,20 +2,27 @@ package com.kallSonys.managedBeans;
 
 import java.io.Serializable;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+
+import com.kallSonys.common.dal.jpa.facade.AddressFacadeLocal;
+import com.kallSonys.common.dal.ldap.mnr.LdapManager;
+import com.kallSonys.seg.biz.transfer.UsersDTO;
 
 @ManagedBean(name="loginPageBean")
 @RequestScoped
 public class LoginPageBean implements Serializable {
 
-	/**
-	 * 
-	 */
+	@EJB(mappedName="AddressBean")
+    private AddressFacadeLocal addressFacadeLocal;	
 	private static final long serialVersionUID = 1L;
-	
+	 
+     
 	private String user;
 	private String password;
+	
+	
 	
 	public LoginPageBean(){}
 	
@@ -23,13 +30,38 @@ public class LoginPageBean implements Serializable {
 	 * Este metodo iniciar la logica 
 	 * para autenticar el usuario
 	 */
-	public String loginAction(){
-		System.out.println("El usuario es:"+this.user);
-		System.out.println("El usuario es:"+this.password);
-		return "exito";
+	public String loginAction()
+	{
+			 
+		LdapManager mnr = new LdapManager();		
+		UsersDTO user = new UsersDTO();		
+		user.setUser(this.getUser());		
+		user.setPass(this.getPassword());		
+		user = mnr.connect(user);
+		if(user.isAutenticado())		
+		{			
+			System.out.println("Usuario logeado, redirigir a la pagina de inicio");
+			pruebaBeanJPA();
+			System.out.println("Ejecutó llamado Bean JPA");
+			return "exito";
+		}
+		
+		System.out.println("No se logró logear el usuario.");
+	
+		return "";
 	}
 	
+	private void pruebaBeanJPA()
+	{
+		
+		//addressFacadeLocal = new  AddressFacadeLocal();
+		//addressBean = new  AddressFacadeLocal();
+		System.out.println("prueba bean 1: ");
+		System.out.println("llamado bean: "+addressFacadeLocal.count());
+		System.out.println("prueba bean 2: ");
+	}
 	
+		
 	public String getUser() {
 		return user;
 	}

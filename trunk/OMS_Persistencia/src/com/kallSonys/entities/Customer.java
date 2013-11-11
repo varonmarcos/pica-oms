14c96e9c50f227e267f81a847d0fd911c6b98625
juan.aranda.galvis@gmail.com <juan.aranda.galvis@gmail.com@24724b2c-733d-aa55-8f23-991fbb591484>
2013-11-11 17:17:48 +0000

    Proyecto de persistencia

---
+++ trunk/OMS_Persistencia/src/com/kallSonys/entities/Customer.java
@@ -0,0 +1,148 @@
+package com.kallSonys.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Set;
+
+
+/**
+ * The persistent class for the CUSTOMER database table.
+ * 
+ */
+@Entity
+public class Customer implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private String custid;
+
+	private String creditcardnumer;
+
+	private String creditcardtype;
+
+	private String email;
+
+	private String fname;
+
+	private String lname;
+
+	private String passwordd;
+
+	private String phonenumber;
+
+	private String status;
+
+	//bi-directional many-to-one association to Customertype
+    @ManyToOne
+	@JoinColumn(name="TYPEID")
+	private Customertype customertype;
+
+	//bi-directional many-to-one association to CustomerAddress
+	@OneToMany(mappedBy="customer")
+	private Set<CustomerAddress> customerAddresses;
+
+	//bi-directional many-to-one association to Order
+	@OneToMany(mappedBy="customer")
+	private Set<Order> orders;
+
+    public Customer() {
+    }
+
+	public String getCustid() {
+		return this.custid;
+	}
+
+	public void setCustid(String custid) {
+		this.custid = custid;
+	}
+
+	public String getCreditcardnumer() {
+		return this.creditcardnumer;
+	}
+
+	public void setCreditcardnumer(String creditcardnumer) {
+		this.creditcardnumer = creditcardnumer;
+	}
+
+	public String getCreditcardtype() {
+		return this.creditcardtype;
+	}
+
+	public void setCreditcardtype(String creditcardtype) {
+		this.creditcardtype = creditcardtype;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFname() {
+		return this.fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return this.lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getPasswordd() {
+		return this.passwordd;
+	}
+
+	public void setPasswordd(String passwordd) {
+		this.passwordd = passwordd;
+	}
+
+	public String getPhonenumber() {
+		return this.phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Customertype getCustomertype() {
+		return this.customertype;
+	}
+
+	public void setCustomertype(Customertype customertype) {
+		this.customertype = customertype;
+	}
+	
+	public Set<CustomerAddress> getCustomerAddresses() {
+		return this.customerAddresses;
+	}
+
+	public void setCustomerAddresses(Set<CustomerAddress> customerAddresses) {
+		this.customerAddresses = customerAddresses;
+	}
+	
+	public Set<Order> getOrders() {
+		return this.orders;
+	}
+
+	public void setOrders(Set<Order> orders) {
+		this.orders = orders;
+	}
+	
+}
---
+++ trunk/OMS_Persistencia/src/com/kallSonys/entities/Item.java
@@ -0,0 +1,95 @@
+package com.kallSonys.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+/**
+ * The persistent class for the ITEMS database table.
+ * 
+ */
+@Entity
+@Table(name="ITEMS")
+public class Item implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private String itemid;
+
+	private String partnum;
+
+	private BigDecimal price;
+
+	private BigDecimal prodid;
+
+	private String productname;
+
+	private BigDecimal quantity;
+
+	//bi-directional many-to-one association to Order
+    @ManyToOne
+	@JoinColumn(name="ORDERID")
+	private Order order;
+
+    public Item() {
+    }
+
+	public String getItemid() {
+		return this.itemid;
+	}
+
+	public void setItemid(String itemid) {
+		this.itemid = itemid;
+	}
+
+	public String getPartnum() {
+		return this.partnum;
+	}
+
+	public void setPartnum(String partnum) {
+		this.partnum = partnum;
+	}
+
+	public BigDecimal getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public BigDecimal getProdid() {
+		return this.prodid;
+	}
+
+	public void setProdid(BigDecimal prodid) {
+		this.prodid = prodid;
+	}
+
+	public String getProductname() {
+		return this.productname;
+	}
+
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+
+	public BigDecimal getQuantity() {
+		return this.quantity;
+	}
+
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+
+	public Order getOrder() {
+		return this.order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+}

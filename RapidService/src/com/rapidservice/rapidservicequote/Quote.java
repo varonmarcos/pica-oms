99c251b55e6f104c3ca51cf92d16de7705b2da0e
juan.aranda.galvis@gmail.com <juan.aranda.galvis@gmail.com@24724b2c-733d-aa55-8f23-991fbb591484>
2013-11-17 08:34:22 +0000

    Initial import.

---
+++ RapidService/src/com/rapidservice/rapidservicequote/Quote.java
@@ -0,0 +1,62 @@
+
+package com.rapidservice.rapidservicequote;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para Quote complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="Quote">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="supplierPrice" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Quote", propOrder = {
+    "supplierPrice"
+})
+public class Quote {
+
+    @XmlElement(required = true, nillable = true)
+    protected String supplierPrice;
+
+    /**
+     * Obtiene el valor de la propiedad supplierPrice.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSupplierPrice() {
+        return supplierPrice;
+    }
+
+    /**
+     * Define el valor de la propiedad supplierPrice.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSupplierPrice(String value) {
+        this.supplierPrice = value;
+    }
+
+}
---
+++ RapidService/src/com/rapidservice/rapidservicequote/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.rapidservice.com/rapidserviceQuote/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.rapidservice.rapidservicequote;

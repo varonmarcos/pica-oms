8db5a75dffb545ad3200160891580c2f001bd0d0
juan.aranda.galvis@gmail.com <juan.aranda.galvis@gmail.com@24724b2c-733d-aa55-8f23-991fbb591484>
2013-11-24 04:48:04 +0000

    Se agrega servicio para consultar ordenes por ID del cliente

--- OMS_Persistencia/src/com/kallSonys/common/dal/jpa/facade/OrdersFacade.java
+++ OMS_Persistencia/src/com/kallSonys/common/dal/jpa/facade/OrdersFacade.java
@@ -4,11 +4,23 @@
  */
 package com.kallSonys.common.dal.jpa.facade;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import com.kallSonys.common.dal.jpa.entitys.AppRecurso;
+import com.kallSonys.common.dal.jpa.entitys.Customer;
 import com.kallSonys.common.dal.jpa.entitys.Orders;
+import com.kallSonys.common.dal.util.Utils;
+
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -29,6 +41,8 @@ public class OrdersFacade extends AbstractFacade<Orders> implements OrdersFacade
         super(Orders.class);
     }
 
+    
+    /*Crea una orden y devuelve el ID generado para la misma*/
     public  String createAndReturnID(Orders orders)
     {
     	
@@ -45,6 +59,53 @@ public class OrdersFacade extends AbstractFacade<Orders> implements OrdersFacade
          	System.out.println("ERROR:OrdersFacade:createAndReturnID: "+e.getMessage());
          	return "-1";
          }
+    }
+    
+    public List<Orders> getOrderByCustomer (String customerID)
+    {
+    
+    	 if(customerID != null && customerID.length()>0)
+		 {
+			 		 		
+	        //Objeto a devolver
+	        List<Orders> setListOrders = new ArrayList<Orders>();
+	         
+	        String sqlNativo = "SELECT CUSTID,ORDERID,ORDERDATE,PRICE,STATUS,COMMENTS "+
+	        					" FROM orders WHERE custid = "+customerID;
+	                	        
+	        Query query = em.createNativeQuery(sqlNativo);
+	        
+	        @SuppressWarnings("unchecked")
+			List<Object[]> listado = query.getResultList();         
+	        if(listado !=null && !listado.isEmpty())
+	        {
+	            for (Iterator<Object[]> it = listado.iterator(); it.hasNext();) 
+	            {
+	                Object[] resultElement = it.next();
+	                
+	                Orders order = new  Orders();
+	                Customer customer = new Customer();
+	                customer.setCustid(customerID);
+	                order.setCustomer(customer);
+	                order.setOrderid(resultElement[1].toString().trim());
+	                order.setOrderdate(Utils.getStringToCalendar(resultElement[2].toString().trim(), "yyyy-MM-dd").getTime());	                 
+	                order.setPrice(new BigDecimal(resultElement[3].toString().trim()));
+	                order.setStatus(resultElement[4].toString().trim());
+	                if(resultElement[5]!=null)
+	                {
+	                	order.setComments(resultElement[5].toString().trim());	
+	                }
+	                else
+	                {
+	                	order.setComments("");
+	                }	                
+	                setListOrders.add(order);	                
+	            }                                                
+	        }
+	                        
+	        return setListOrders;
+		 }		 	
+    	return null;
     }
     
 }
